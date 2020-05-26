35c72c07a6ec3650cb07b5ca5d3267f8fa49f858
Dhruv <dhruvkapoor.200@gmail.com>
2020-05-26 14:19:12 +0530

    Initial commit

---
+++ app/src/main/java/com/example/ipunotes/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.ipunotes
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.system.Os.close
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setSupportActionBar(toolbar)
+        val drawerToggle = ActionBarDrawerToggle(this, drawer, toolbar, R.string.open, R.string.close)
+        drawer.addDrawerListener(drawerToggle)
+        drawerToggle.syncState()
+
+        supportFragmentManager.beginTransaction().replace(R.id.container, MainFragment()).commit()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_manu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.settingsOption->{
+                startActivity(Intent(this, SettingsActivity::class.java))
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
---
+++ app/src/main/java/com/example/ipunotes/SettingsActivity.kt
@@ -0,0 +1,24 @@
+package com.example.ipunotes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.PreferenceManager
+
+class SettingsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+
+        supportFragmentManager.beginTransaction().replace(R.id.settingsContainer, SettingsFragment()).commit()
+    }
+
+
+    class SettingsFragment : PreferenceFragmentCompat(),PreferenceManager.OnPreferenceTreeClickListener{
+        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+            setPreferencesFromResource(R.xml.preferences, rootKey)
+        }
+
+    }
+}
