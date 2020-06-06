2172a0392e5cb28897437aba018dff27d0735e3b
Dhruv <dhruvkapoor.200@gmail.com>
2020-06-06 17:44:39 +0530

    added on click for files, fixed no content screen

--- app/src/main/java/com/example/ipunotes/adapters/FilesAdapter.kt
+++ app/src/main/java/com/example/ipunotes/adapters/FilesAdapter.kt
@@ -9,31 +9,50 @@ import com.example.ipunotes.R
 import com.example.ipunotes.models.File
 import kotlinx.android.synthetic.main.file_item_view.view.*
 
-class NotesAdapter(private val list: List<File>) : RecyclerView.Adapter<NotesViewHolder>(){
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NotesViewHolder =
-        NotesViewHolder(
+class FilesAdapter(
+    private val filesAdapterListeners: FilesAdapterListeners,
+    private val list: List<File>
+) : RecyclerView.Adapter<FileViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FileViewHolder =
+        FileViewHolder(
+            filesAdapterListeners,
             LayoutInflater
                 .from(parent.context)
                 .inflate(R.layout.file_item_view, parent, false)
         )
 
-    override fun getItemCount(): Int = list.size
+    override fun getItemCount(): Int {
+        filesAdapterListeners.isContentEmpty(list.isEmpty())
+        return list.size
+    }
 
-    override fun onBindViewHolder(holder: NotesViewHolder, position: Int) {
-            holder.bind(list[position])
+    override fun onBindViewHolder(holder: FileViewHolder, position: Int) {
+        holder.bind(list[position])
     }
 
 }
 
-class NotesViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
-    fun bind(note: File){
-        with(itemView){
+class FileViewHolder(private val filesAdapterListeners: FilesAdapterListeners, itemView: View) :
+    RecyclerView.ViewHolder(itemView),View.OnClickListener {
+    fun bind(note: File) {
+        with(itemView) {
             tvFileName.text = note.name
             val authorText = "Author: ${note.author}"
             tvAuthor.text = authorText
             val uploadDate = "Upload Date: ${note.upload_date}"
             tvUploadDate.text = uploadDate
             tvLabel.text = Extras.getLabel(note.name)
+
+            setOnClickListener(this@FileViewHolder)
         }
     }
+
+    override fun onClick(v: View?) {
+        filesAdapterListeners.onFileClick(adapterPosition)
+    }
+}
+
+interface FilesAdapterListeners {
+    fun onFileClick(position: Int)
+    fun isContentEmpty(value: Boolean)
 }
